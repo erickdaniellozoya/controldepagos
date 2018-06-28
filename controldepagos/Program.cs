54432c7f6f403e41eda6b9049b33b875128509f4
Erick Daniel Lozoya Rosas <34018215+ericklozoya@users.noreply.github.com>
2018-06-27 21:10:52 -0500

    Form de inicio
    Se cambio el form con el que inicia el programa

--- controldepagos/Program.cs
+++ controldepagos/Program.cs
@@ -16,7 +16,7 @@ namespace controldepagos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Reportecs());
+            Application.Run(new login());
         }
     }
 }
